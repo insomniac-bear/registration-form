f7945adf4126f000b3a4e8a7c59ad381220937c6
unknown <tcherepanov.kir@gmail.com>
2022-04-25 02:27:58 +0300

    Updates

---
+++ precache-manifest.7c358de0498183669dcac5c7a5986b6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54db7ddaed1a7e885b81",
+    "url": "/static/css/main.8a96d3f2.chunk.css"
+  },
+  {
+    "revision": "54db7ddaed1a7e885b81",
+    "url": "/static/js/main.54db7dda.chunk.js"
+  },
+  {
+    "revision": "2e8f1a6b9f28a667d85d",
+    "url": "/static/js/1.2e8f1a6b.chunk.js"
+  },
+  {
+    "revision": "8cf577de77b01864168e",
+    "url": "/static/js/2.8cf577de.chunk.js"
+  },
+  {
+    "revision": "ca15752fc42e5c2f840d",
+    "url": "/static/js/runtime~main.ca15752f.js"
+  },
+  {
+    "revision": "27a993b7b7495a13c414dfdc2b0f90a2",
+    "url": "/index.html"
+  }
+];
